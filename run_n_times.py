4a5401518efa5e3e29c764dc3bedd3628b7a4480
lbuyi97 <84145016+lbuyi97@users.noreply.github.com>
2023-06-19 15:11:23 -0300

    Update run_n_times.py

--- run_n_times.py
+++ run_n_times.py
@@ -8,7 +8,4 @@ output_prefix = "out"
 n_iter = 100
 
 for i in range(n_iter):
-    #output_file = output_prefix + '_' + str(i) + '.txt'
-    #sys.stdout = open(output_file, 'w')
-    #subprocess.call(['python', script_name], stdout=sys.stdout, stderr=subprocess.STDOUT)
     os.system("python3 "+ script_name + " " + str(i) )
